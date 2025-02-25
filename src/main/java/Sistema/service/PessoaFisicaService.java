5be01aef5ef32ef6f55fc4b6078985b10af3cb52
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-02-25 14:43:35 -0300

    .

--- src/main/java/Sistema/service/PessoaFisicaService.java
+++ src/main/java/Sistema/service/PessoaFisicaService.java
@@ -7,22 +7,25 @@ public class PessoaFisicaService {
     private static double receita;
 
     public static double adicionaSalario(double salario) {
-        if (salario < 0) throw new IllegalArgumentException("o salario nao pode ser menor que zero ");
+        returnsAnExceptionIfValueIsLessThanZero(salario);
         salarioCalculado = salario;
         return salario;
     }
     public static double adicionaReceita(double valor) {
-        if (valor < 0 ) throw new IllegalArgumentException("receita nao pode ser menor que 0");
+        returnsAnExceptionIfValueIsLessThanZero(valor);
         receita += valor;
         return receita;
     }
     public static double calculaGastos(double gastos) {
-        if (gastos < 0 ) throw new IllegalArgumentException("gastos nao podem ser negativos");
+        returnsAnExceptionIfValueIsLessThanZero(gastos);
         gastosCalculado += gastos;
         return gastosCalculado;
     }
     public static double calculaSaldoDaConta() {
        saldo = (receita + salarioCalculado) - gastosCalculado;
        return saldo;
+    }
+    private static void returnsAnExceptionIfValueIsLessThanZero(double valor) {
+        if (valor < 0 ) throw new IllegalArgumentException("receita nao pode ser menor que 0");
     }
 }
